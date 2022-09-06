8eb74aef742a9f8ad93b82d91b719691ae3a85d1
太田彰造 <3aos4682@gmail.com>
2022-09-06 19:56:31 +0900

    ユーザーと投稿を紐付け

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
 
+    belongs_to :user
+
     def display_created_at
         I18n.l(self.created_at, format: :default)
     end

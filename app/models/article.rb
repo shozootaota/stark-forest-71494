d9f14f4ce455f5f78eddea896ec6c95c91c9fdfd
太田彰造 <3aos4682@gmail.com>
2022-09-08 03:01:32 +0900

    いいね機能追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,7 @@ class Article < ApplicationRecord
     validates :content, presence: true
 
     has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
     belongs_to :user
 
     def display_created_at

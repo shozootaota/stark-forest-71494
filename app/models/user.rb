45353dd34b425b55e91ecd86f0532e8b1394e4d0
太田彰造 <3aos4682@gmail.com>
2022-09-10 15:43:13 +0900

    プロフィール編集・更新できるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,32 +25,34 @@ class User < ApplicationRecord
   end
 
   def avatar_image
-    profile&.avatar || 'default-avatar.png'
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
   end
 
-  def follow!(user_id:)
-    if user.is_a?(User)
-      user_id = user.id
-    else
-      user_id = user
-    end
-
-    following_relationships.create!(following_id: user.id)
+  def follow!(user)
+    user_id = get_user_id(user)
+    following_relationships.create!(following_id: user_id)
   end
 
   def unfollow!(user)
-    if user.is_a?(User)
-      user_id = user.id
-    else
-      user_id = user
-    end
-
+    user_id = get_user_id(user)
     relation = following_relationships.find_by!(following_id: user_id)
-    relation.destroy
+    relation.destroy!
   end
 
   def has_followed?(user)
     following_relationships.exists?(following_id: user.id)
   end
 
+  private
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
+    else
+      user
+    end
+  end
 end

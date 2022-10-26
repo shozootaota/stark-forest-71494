fab4da1a3dae4f2878a5e79b12b4aae07eb73077
MACBOOK <>
2022-10-27 08:10:51 +0900

    コメントした人のニックネーム・プロフィール画像を表示できるようにした

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,10 +12,11 @@ class CommentsController < ApplicationController
     def create
         article = Article.find(params[:article_id])
         @comment = article.comments.build(comment_params)
+        @comment.user_id  = current_user.id
         if @comment.save
             redirect_to article_path(article), notice: 'コメントを追加'
         else
-            flash.now[:error] = '更新できませんでした'
+            flash.now[:error] = '投稿できませんでした'
             render :new
         end
     end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :article
-    belongs_to :user
+    belongs_to :user # 追加
     validates :content, presence: true
     validates :content, length: { minimum: 2 }
 end
